e71c7ef13d90f285dd8d063c60a00b1a1338fd59
Natalia Teaca <lilikoy@sbcglobal.net>
2020-06-13 00:21:09 -0700

    questions area set

---
+++ Assets/test.js
@@ -0,0 +1,5 @@
+var intro = document.querySelector(".criteriapage");
+var crit = document.querySelector(".quiz-criteria");
+var startBtn = document.querySelector('startedBtn');
+//startBtn.classList.add(".startedBtn:hover");
+var timer = document.getElementById("timer");
